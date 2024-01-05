872ba358a09fe384b1d1e60ff17d9711c85b4c22
Douglas Marins <dmv@tokenizadora.com.br>
2024-01-05 19:34:25 -0300

    Exemplo de autenticação http basica de usuario, utilizando o browser

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+  # Exemplo de autenticação http basica de usuario, utilizando o browser
+  # http_basic_authenticate_with name: "admin", password: "admin"
+
   def index
   end
 end
