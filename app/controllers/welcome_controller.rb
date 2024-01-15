e1fb0e6c926938cad0a58245cbf8046a14690268
Douglas Marins <dmv@tokenizadora.com.br>
2024-01-15 12:40:40 -0300

    Concluindo controllers

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  # Exemplo de autenticação http basica de usuario, utilizando o browser
-  # http_basic_authenticate_with name: "admin", password: "admin"
-
-  def index
-  end
-end
